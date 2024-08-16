526f563001c2e4d5ac6009ace6fe3dd1b704dc38
farhan-alam18 <farhanalam1113@gmail.com>
2024-08-16 16:50:26 +0530

    Deploying-IV

--- components/header.tsx
+++ components/header.tsx
@@ -10,7 +10,7 @@ export const Header = () => {
     <header className="bg-gradient-to-b from-blue-700 to-blue-700 px-4 lg:px-14 py-8 pb-36">
         <div className="max-w-screen-2xl mx-auto">
             <div className="w-full flex items-center justify-between mb-14">
-                <div className="flex items-center lg:gap-x-16">
+                <div className="flex flex-row-reverse lg:flex items-center gap-x-32 lg:gap-x-16">
                     <HeaderLogo/>
                     <Navigation/>
                 </div>
--- components/headerlogo.tsx
+++ components/headerlogo.tsx
@@ -4,9 +4,9 @@ import Image from "next/image"
 export const HeaderLogo = () => {
     return(
         <Link href='/'>
-            <div className="items-center hidden lg:flex">
+            <div className="items-center flex">
                 <Image src='/logo.svg' alt="Logo" height={28} width={28}/>
-                <p className="font-semibold text-white text-2xl ml-2.5">
+                <p className="font-semibold text-white lg:text-2xl ml-2.5">
                     Financio
                 </p>
             </div>
